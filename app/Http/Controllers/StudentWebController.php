45b913c47c7aaf7a0e801969c0c2cefbdbcfee43
Rahul <easyqueteam@gmail.com>
2022-12-19 12:46:08 +0530

    creating view files & code with controller

--- app/Http/Controllers/StudentWebController.php
+++ app/Http/Controllers/StudentWebController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class StudentWebController extends Controller
 {
-    //
+    public function index()
+    {
+        // dd("index");
+
+        return view('students.index');
+    }
+    public function add()
+    {
+        // dd("add student");
+        return view('students.add');
+    }
+    public function store()
+    {
+        $validate  = $request->validate([
+            'name' => 'required|max:255',
+            'class' => 'required|max:255',
+            'roll_number' => 'required',
+        ]);
+        $student = new Student;
+        $student->name = 
+    }
 }
