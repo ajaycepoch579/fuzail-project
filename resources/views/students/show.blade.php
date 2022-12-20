6bc006a43083317561e896aa4eb8c12877b74275
Rahul <easyqueteam@gmail.com>
2022-12-20 11:50:39 +0530

    code commit

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> Show User</h2>
+                <h2> Show Student</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('students.index') }}"> Back</a>
@@ -20,6 +20,7 @@
           <td>Name</td>
           <td>Class</td>
           <td>Roll Number</td>
+          <td>Image</td>
           <td class="text-center">Action</td>
         </tr>
     </thead>
@@ -29,6 +30,7 @@
             <td>{{$students->name}}</td>
             <td>{{$students->class}}</td>
             <td>{{$students->roll_number}}</td>
+            <td></td>
             <td class="text-center">
                 <a href="{{ route('students.edit', $students->id)}}" class="btn btn-primary btn-sm"">Edit</a>
                 <form action="{{ route('students.destroy', $students->id)}}" method="post" style="display: inline-block">
@@ -38,7 +40,6 @@
                   </form>
             </td>
         </tr>
-        @endforeach
     </tbody>
   </table>
     </div>
