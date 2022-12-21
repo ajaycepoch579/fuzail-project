fa8ea53036372788dbed044d04f71666c34a46f4
Rahul <easyqueteam@gmail.com>
2022-12-21 18:12:35 +0530

    add multiple images

--- app/Http/Controllers/StudentWebController.php
+++ app/Http/Controllers/StudentWebController.php
@@ -36,7 +36,6 @@ class StudentWebController extends Controller
             if($student->save())
             {
                 if ($request->hasFile('image')) {
-
                     $image = $request->file('image');
                     // $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
                     $filename = time().'.'.request()->image->getClientOriginalExtension();
@@ -49,7 +48,7 @@ class StudentWebController extends Controller
                 }
             }
         
-        return redirect('/students')->with('completed', 'Student has been saved!');
+        return redirect('/students')->with('success', 'New Student is Added.');
     }
     public function edit($id)
     {
@@ -60,7 +59,7 @@ class StudentWebController extends Controller
     public function show($id)
     {
         $students = Student::find($id);
-        $image = StudentImage::where('stu_id',$id)->first();
+        $image = StudentImage::where('stu_id',$id)->latest()->first();
         // dd($img->image);
 
         // echo "<img src="/storage/images/$image-" />";
@@ -69,19 +68,73 @@ class StudentWebController extends Controller
 
     public function update(Request $request, $id)
     {
-        $validate  = $request->validate([
+        $request->validate([
             'name' => 'required|max:255',
             'class' => 'required|max:255',
             'roll_number' => 'required',
         ]);
-        Student::whereId($id)->update($validate);
-        return redirect('/students')->with('completed', 'Student has been updated');
+        $requestData = $request->all();
+    
+            $studentObj = Student::find($id);
+            $studentObj->name        = $requestData['name'] ?? $studentObj->name;
+            $studentObj->class       = $requestData['class'] ?? $studentObj->class;
+            $studentObj->roll_number = $requestData['roll_number'] ?? $studentObj->roll_number;
+            $studentObj->save();
+                if($studentObj->save())
+                {
+                    if ($request->hasFile('image')) 
+                    {
+                        $image = $request->file('image');
+                        // $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
+                        $filename = time().'.'.request()->image->getClientOriginalExtension();
+                        request()->image->move(public_path('images'), $filename);
+                            // $request->image->storeAs('images', $filename);
+                            $stuImg = new StudentImage();
+                            $stuImg->stu_id = $id;
+                            $stuImg->image=$filename;
+                            $stuImg->save();
+                    }
+                }
+            // $studentObj->employee_no = $requestData['employee_no'] ?? $filterObj->employee_no;
+        // $student = Student::whereId($id)->update($validate);
+        return redirect('/students')->with('success', 'Student has been updated');
     }
 
     public function destroy($id)
     {
         $student = Student::findOrFail($id);
         $student->delete();
-        return redirect('/students')->with('completed', 'Student has been deleted');
+        return redirect('/students')->with('success', 'Student has been deleted');
+    }
+    public function addImage($id)
+    {
+        $students = $id;
+        return view('students.addimage',compact('students'));
+    }
+    public function saveImage(Request $request , $id)
+    {
+        $request->validate([
+            // 'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+            'image' => 'required',
+            'image.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);  
+        // $allowedfileExtension=['pdf','jpg','png'];
+        
+        if ($request->hasFile('image')) {
+            $images = $request->file('image');       
+            foreach($images as $image){
+                // dd($image);
+            $filename = $image->getClientOriginalName();
+            // $extension = $image->getClientOriginalExtension();
+            // dd($extension);
+            $image->move(public_path('images'), $filename);
+                // $request->image->storeAs('images', $filename);
+                $stuImg = new StudentImage();
+                $stuImg->stu_id = $id;
+                $stuImg->image=$filename;
+                $stuImg->save();
+            }
+        }
+        return redirect('/students')->with('success', 'Images Added Successfully.');
     }
 }
